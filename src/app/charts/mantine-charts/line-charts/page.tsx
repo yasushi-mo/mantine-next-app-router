60886ef6e1caeb94be6485f1ce01092d01c64d49
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-09-06 09:49:36 +0900

    Update: Style

--- src/app/charts/mantine-charts/line-charts/page.tsx
+++ src/app/charts/mantine-charts/line-charts/page.tsx
@@ -12,6 +12,7 @@ export default function LineCharts() {
   return (
     <LineChart
       h={400}
+      w={600}
       data={data}
       dataKey="name"
       series={[{ name: "value", color: "blue" }]}
