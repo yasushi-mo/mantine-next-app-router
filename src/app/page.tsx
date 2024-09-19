16a0fec7e0df5c0c5c34494ccdcf0288114c9ee5
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-09-20 08:25:14 +0900

    Update: Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
-import { Button, Container, Group, Space } from "@mantine/core";
+import { Button, Container, Group, Stack } from "@mantine/core";
 
 export default function Home() {
   return (
     <Container m="md">
-      <Group>
-        <Button>Default Button</Button>
-        <Button color="red" variant="filled">
-          Button with props
-        </Button>
-      </Group>
-      <Space h="xl" />
-      <Group>
-        <Button size="xxl">XXL Button</Button>
-        <Button size="xxs">XXS Button</Button>
-      </Group>
+      <Stack gap="md">
+        <Group>
+          <Button>Default Button</Button>
+          <Button color="red" variant="filled">
+            Button with props
+          </Button>
+        </Group>
+        <Group>
+          <Button size="xxl">XXL Button</Button>
+          <Button size="xxs">XXS Button</Button>
+        </Group>
+      </Stack>
     </Container>
   );
 }
