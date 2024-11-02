a21f38cdbd9c8e31ffd212710866299ed8e3b185
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-11-03 08:52:14 +0900

    Update: styles

--- src/app/charts/mantine-charts/bar-charts/top-to-bottom/page.tsx
+++ src/app/charts/mantine-charts/bar-charts/top-to-bottom/page.tsx
@@ -2,11 +2,11 @@ import { BarChart } from "@mantine/charts";
 
 const data = [
   { month: "January", gap: 20 },
-  { month: "February", gap: 90 },
+  { month: "February", gap: 60 },
   { month: "March", gap: 40 },
   { month: "April", gap: 100 },
-  { month: "May", gap: 80 },
-  { month: "June", gap: 75 },
+  { month: "May", gap: 50 },
+  { month: "June", gap: 70 },
 ];
 
 export default function Page() {
@@ -20,7 +20,7 @@ export default function Page() {
         reversed: true,
       }}
       xAxisProps={{
-        orientation: "top", // Positions x-axis at the top if this property is available
+        orientation: "top",
       }}
     />
   );
