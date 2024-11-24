e9857b83ca38615c70c21245bb1abaafae47eb61
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-11-24 09:11:47 +0900

    Add: Vertical composite chart

---
+++ src/app/charts/mantine-charts/composite-charts/vertical/page.tsx
@@ -0,0 +1,53 @@
+import { CompositeChart } from "@mantine/charts";
+
+export const data = [
+  {
+    date: "Mar 22",
+    Apples: 2890,
+    Oranges: 2338,
+    Tomatoes: 2452,
+  },
+  {
+    date: "Mar 23",
+    Apples: 2756,
+    Oranges: 2103,
+    Tomatoes: 2402,
+  },
+  {
+    date: "Mar 24",
+    Apples: 3322,
+    Oranges: 986,
+    Tomatoes: 1821,
+  },
+  {
+    date: "Mar 25",
+    Apples: 3470,
+    Oranges: 2108,
+    Tomatoes: 2809,
+  },
+  {
+    date: "Mar 26",
+    Apples: 3129,
+    Oranges: 1726,
+    Tomatoes: 2290,
+  },
+];
+
+export default function CompositeCharts() {
+  return (
+    <CompositeChart
+      h={300}
+      data={data}
+      dataKey=""
+      maxBarWidth={30}
+      series={[
+        { name: "Tomatoes", color: "rgba(18, 120, 255, 0.2)", type: "bar" },
+        { name: "Apples", color: "red.8", type: "line" },
+        { name: "Oranges", color: "yellow.8", type: "area" },
+      ]}
+      composedChartProps={{ layout: "vertical" }}
+      xAxisProps={{ type: "number" }}
+      yAxisProps={{ type: "category", dataKey: "date" }}
+    />
+  );
+}
