8c9a8af00bb5534ad17607021cfbc6bac93b64db
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-12-10 08:21:45 +0900

    Add: barchart data

---
+++ src/app/charts/d3/bar-charts/data.ts
@@ -0,0 +1,14 @@
+export const data = [
+  { month: "January", income: 80 },
+  { month: "February", income: 10 },
+  { month: "March", income: 40 },
+  { month: "April", income: 30 },
+  { month: "May", income: 40 },
+  { month: "June", income: 50 },
+  { month: "July", income: 60 },
+  { month: "August", income: 70 },
+  { month: "September", income: 80 },
+  { month: "October", income: 40 },
+  { month: "November", income: 60 },
+  { month: "December", income: 90 },
+];
