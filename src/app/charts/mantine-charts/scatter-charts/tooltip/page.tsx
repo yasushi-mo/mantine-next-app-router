0a5ae70d3a80246fbf2d34a6952a258c3e897332
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-12-13 08:09:40 +0900

    Add: Tooltip

---
+++ src/app/charts/mantine-charts/scatter-charts/tooltip/page.tsx
@@ -0,0 +1,14 @@
+import { ScatterChart } from "@mantine/charts";
+import { data } from "./data";
+
+export default function Page() {
+  return (
+    <ScatterChart
+      h={360}
+      data={data}
+      dataKey={{ x: "age", y: "BMI" }}
+      xAxisLabel="Age"
+      yAxisLabel="BMI"
+    />
+  );
+}
