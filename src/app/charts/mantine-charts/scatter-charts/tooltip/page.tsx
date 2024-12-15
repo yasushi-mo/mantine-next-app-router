1b168e29100fbca37a3d5b1e5350b5dd2c7f1696
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-12-15 19:42:13 +0900

    Delete: axis label

--- src/app/charts/mantine-charts/scatter-charts/tooltip/page.tsx
+++ src/app/charts/mantine-charts/scatter-charts/tooltip/page.tsx
@@ -2,13 +2,5 @@ import { ScatterChart } from "@mantine/charts";
 import { data } from "./data";
 
 export default function Page() {
-  return (
-    <ScatterChart
-      h={360}
-      data={data}
-      dataKey={{ x: "age", y: "BMI" }}
-      xAxisLabel="Age"
-      yAxisLabel="BMI"
-    />
-  );
+  return <ScatterChart h={360} data={data} dataKey={{ x: "age", y: "BMI" }} />;
 }
